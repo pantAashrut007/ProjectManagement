a6f486ab47ec616cba9778c14a075b1fdd1fe54e
Aashrut Pant <aashrut.pant@gmail.com>
2020-09-21 14:37:20 +0530

    first commit

---
+++ projectManagement/projectManagement/src/test/java/com/bottomline/projectManagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottomline.projectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
